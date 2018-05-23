008d19d308a8f169c879b1192f3240c8b28b8d9f
南宫封清 <1390811049@qq.com>
2018-05-23 22:33:40 +0800

    test1

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: UserController
+ * Author:   Administrator
+ * Date:     2018/5/23/023 22:31
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.mmall.controller.portal;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2018/5/23/023
+ * @since 1.0.0
+ */
+public class UserController {
+
+}
